67d508b2797ffce4970ac1bd17af5cd24fd6f0b3
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2023-04-24 11:51:02 +0500

    ✏️ Added detail view

--- apply/urls.py
+++ apply/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import FacultyApiView, TicketApiView
+from .views import FacultyApiView, FacultyDetailApiView, TicketApiView, TicketDetailApiView
 
 
 urlpatterns = [
     path('faculties/', FacultyApiView.as_view()),
+    path('faculties/<int:pk>/', FacultyDetailApiView.as_view()),
     path('tickets/', TicketApiView.as_view()),
+    path('tickets/<int:pk>', TicketDetailApiView.as_view()),
 ]
--- apply/views.py
+++ apply/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import Ticket, Faculty
 from .serializers import FacultySerializer, TicketSerializer
 
@@ -8,6 +8,16 @@ class TicketApiView(ListAPIView):
     serializer_class = TicketSerializer
 
 
+class TicketDetailApiView(RetrieveAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
 class FacultyApiView(ListAPIView):
+    queryset = Faculty.objects.all()
+    serializer_class = FacultySerializer
+
+
+class FacultyDetailApiView(RetrieveAPIView):
     queryset = Faculty.objects.all()
     serializer_class = FacultySerializer
